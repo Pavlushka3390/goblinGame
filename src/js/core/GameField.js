ba0d12215a337460e17d8b26b00216def821ee0d
Pavlushka3390 <pavlushka3390@mail.ru>
2022-07-09 10:42:18 +0300

    !

--- src/js/core/GameField.js
+++ src/js/core/GameField.js
@@ -21,6 +21,7 @@ export default class GameField {
 
   init() {
     this.field = Array(this.rowCount)
-      .fill(0).map((_, row) => Array(this.columnCount).fill(0).map((_,column) => new this.CellClass({ row, column })));
+      .fill(0).map((a, row) => Array(this.columnCount)
+      .fill(0).map((b, column) => new this.CellClass({ row, column })));
   }
 }
